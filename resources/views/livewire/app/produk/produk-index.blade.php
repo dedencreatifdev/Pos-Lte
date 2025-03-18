aed20746af4ca784eadca38b6961e965d0cf6f5e
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-03-19 04:47:28 +0700

    .

--- resources/views/livewire/app/produk/produk-index.blade.php
+++ resources/views/livewire/app/produk/produk-index.blade.php
@@ -3,7 +3,40 @@
         <div class="col">
             <div class="card">
                 <div class="card-header"></div>
-                <div class="card-body"></div>
+                <div class="p-0 card-body">
+                    <table class="table table-sm table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Kode Barang</th>
+                                <th>Nama Barang</th>
+                                <th>Satuan</th>
+                                <th>Kategori</th>
+                                <th>Merk</th>
+                                <th>Harga</th>
+                                <th>Stok</th>
+                                <th>Hpp</th>
+                                <th>Alert</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>MB220900</td>
+                                <td>Filter Solar</td>
+                                <td>Pcs</td>
+                                <td>Fuel</td>
+                                <td>MMKSI</td>
+                                <td>65.000</td>
+                                <td>20</td>
+                                <td>15.000</td>
+                                <td>5</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
